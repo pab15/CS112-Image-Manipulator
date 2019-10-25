e4e5fde205de5779536d96ed6c00817a0e195b39
pab15 <>
2019-10-25 15:51:50 -0700

    Update File Helpers

--- FileHelpers.h
+++ FileHelpers.h
@@ -5,12 +5,10 @@
 #include <vector>
 #include <string>
 #include <fstream>
-#include "PpmClass.hpp"
 using namespace std;
 
 vector<string> readFile(ifstream& some_file);
 vector<string> readFile(const string& file_name);
-bool countinueFuction(char c);
-void menuSwitch(char c, string input_file, string output_file, PpmClass doc);
+
 
 #endif
